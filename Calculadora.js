8c633b7af6e06616b1bee29c9c9262b903426131
BrunoGTI <brunoh14pb@gmail.com>
2019-10-24 23:06:32 -0300

    Versão 0.1

---
+++ Calculadora.js
@@ -0,0 +1,12 @@
+var mostrar = visor;
+
+function botao( num ){
+	var salva = document.Calc.visor.value;
+	document.Calc.visor.value = salva + num;
+}
+function rasultado (){
+	var resul = document.Calc.visor;
+	if{
+		document.Calc.visor = eval(resul);
+	}
+}
